4ec25a0f37eb71d79e98c20be683eb077aa39d13
Neko014 <mavememije@gmail.com>
2024-11-27 15:05:47 +0800

    Connected Main menu to Game Scene

--- Main_Menu/main_menu.gd
+++ Main_Menu/main_menu.gd
@@ -1,10 +1,7 @@
 extends Control
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
 func _on_exit_pressed() -> void:
 	get_tree().quit()
+
+func _on_play_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/game_scene.tscn")
