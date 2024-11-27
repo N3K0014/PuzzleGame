55596bc8bb8ba3fcc99d7351aa4e0ce554fb8017
Neko014 <mavememije@gmail.com>
2024-11-28 00:43:51 +0800

    More Updates
    + Change box gdscript movement universal script
    + Background
    + More Goal Spots
    + made the Square Block

--- Scenes/Objects/square_spot.gd
+++ Scenes/Objects/square_spot.gd
@@ -3,9 +3,9 @@ extends Area2D
 var occupied = false 
 
 func _on_body_entered(body: Node2D) -> void:
-	if body.is_in_group('box'):
+	if body.is_in_group('SquareBox'):
 		occupied = true
 
 func _on_body_exited(body: Node2D) -> void:
-	if body.is_in_group('box'):
+	if body.is_in_group('SquareBox'):
 		occupied = false
